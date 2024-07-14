0d9dc089b46a041e864c67a05024d40ff0f16102
GalibIbr <galibiavi4@gmail.com>
2024-07-14 16:10:29 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react(), splitVendorChunkPlugin()],
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-        },
-      },
-    },
     base: '/',
   }
 
