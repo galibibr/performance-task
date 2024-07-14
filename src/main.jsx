c8794d32d372bd5466259f482eaa4450a0b14a8a
GalibIbr <galibiavi4@gmail.com>
2024-07-14 14:32:16 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
-import './reset.css'
-import './main.css'
+import Header from './components/Header'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <>
+    <Header/>
+    <App/>
+  </>
 )
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   base: "/performance-task/"
 })
